fa38516a9756a02e2a15567e1e70a20072ae6f5f
= <=>
2023-12-03 00:18:50 +0800

    fix: 增加提示

--- components/slider-tool.tsx
+++ components/slider-tool.tsx
@@ -10,6 +10,32 @@ import {
   BiHomeAlt,
 } from "react-icons/bi";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const TooltipString = ({
+  children,
+  tooltip,
+}: {
+  children: React.ReactNode;
+  tooltip: string;
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>
+          <p>{tooltip}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
 const SliderTool: React.FC = () => {
   const hasMounted = useHasMounted();
   const { theme, setTheme } = useTheme();
@@ -18,35 +44,43 @@ const SliderTool: React.FC = () => {
   }
   return (
     <div className="fixed right-2 sm:right-5 bottom-12 flex flex-col gap-4">
-      <Link href="/">
+      <TooltipString tooltip="主页">
+        <Link href="/">
+          <Button size="icon" className="rounded-full">
+            <BiHomeAlt className="w-5 h-5" />
+          </Button>
+        </Link>
+      </TooltipString>
+      <TooltipString tooltip="网站设置">
+        <Link href="/setting">
+          <Button size="icon" className="rounded-full">
+            <AiOutlineSetting className="w-5 h-5" />
+          </Button>
+        </Link>
+      </TooltipString>
+      <TooltipString tooltip="主题切换">
         <Button size="icon" className="rounded-full">
-          <BiHomeAlt className="w-5 h-5" />
+          {theme === "dark" ? (
+            <BiSolidSun className="w-5 h-5" onClick={() => setTheme("light")} />
+          ) : (
+            <BiSolidMoon className="w-5 h-5" onClick={() => setTheme("dark")} />
+          )}
         </Button>
-      </Link>
-      <Link href="/setting">
-        <Button size="icon" className="rounded-full">
-          <AiOutlineSetting className="w-5 h-5" />
+      </TooltipString>
+      <TooltipString tooltip="回到顶部">
+        <Button
+          size="icon"
+          className="rounded-full"
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: "smooth",
+            })
+          }>
+          <BiArrowToTop className="w-5 h-5" />
         </Button>
-      </Link>
-      <Button size="icon" className="rounded-full">
-        {theme === "dark" ? (
-          <BiSolidSun className="w-5 h-5" onClick={() => setTheme("light")} />
-        ) : (
-          <BiSolidMoon className="w-5 h-5" onClick={() => setTheme("dark")} />
-        )}
-      </Button>
-      <Button
-        size="icon"
-        className="rounded-full"
-        onClick={() =>
-          window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          })
-        }>
-        <BiArrowToTop className="w-5 h-5" />
-      </Button>
+      </TooltipString>
     </div>
   );
 };
